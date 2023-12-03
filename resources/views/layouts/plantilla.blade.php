c53d3a515b3dfec816a4525bcaa4990cb48576f9
Asdru Díaz <125107126+asdruds@users.noreply.github.com>
2023-12-02 19:04:37 -0600

    Ajustes

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,8 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nombre de tu Aplicación</title>
-    <!-- Agrega tus estilos CSS aquí -->
+    <title>Evidencia 3</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
@@ -21,18 +20,12 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('courses.index') }}">Cursos</a>
                 </li>
-                <!-- Puedes agregar más elementos del menú aquí -->
             </ul>
         </div>
     </nav>
 
     <div class="container mt-4">
-        @yield('content') <!-- Aquí se incluirá el contenido de las vistas -->
+        @yield('content') 
     </div>
-
-    <!-- Agrega tus scripts JavaScript aquí -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
